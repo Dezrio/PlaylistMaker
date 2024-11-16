ad94b4095c3bc5a85ed37a2de7c9b7742957daf3
rosinskiyda <rosinskiyda@intellect-soft.ru>
2024-11-16 23:12:50 +0400

    add_button_actions

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,20 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<Button>(R.id.searchBtn).setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Выполнение поиска...", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        findViewById<Button>(R.id.libraryBtn).setOnClickListener {
+            Toast.makeText(this@MainActivity, "Открытие мидиатеки", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<Button>(R.id.settingsBtn).setOnClickListener {
+            Toast.makeText(this@MainActivity, "Настройка", Toast.LENGTH_SHORT).show()
         }
     }
 }
