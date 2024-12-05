ed4ec08462410c55aad057bfd05381351c750e7e
rosinskiyda <rosinskiyda@intellect-soft.ru>
2024-12-05 20:20:16 +0400

    Спринт №9. Домашняя работа №2.

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +19,28 @@ class SettingsActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<ImageView>(R.id.share).setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.setType("text/plain")
+            shareIntent.putExtra(Intent.EXTRA_TEXT, R.string.curs_uri)
+            startActivity(shareIntent)
+        }
+
+        findViewById<ImageView>(R.id.support).setOnClickListener {
+            val sendIntent = Intent(Intent.ACTION_SENDTO)
+            sendIntent.data = Uri.parse("mailto:")
+            sendIntent.putExtra(Intent.EXTRA_SUBJECT, R.string.support_subject)
+            sendIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(R.string.support_email))
+            sendIntent.putExtra(Intent.EXTRA_TEXT, R.string.support_message)
+            startActivity(sendIntent)
+        }
+
+        findViewById<ImageView>(R.id.arrow_forward).setOnClickListener {
+            val offerIntent = Intent(Intent.ACTION_VIEW)
+            offerIntent.data = Uri.parse(R.string.offer_uri.toString())
+            startActivity(offerIntent)
         }
 
         findViewById<ImageView>(R.id.settings_arrow_back).setOnClickListener {
