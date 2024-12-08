97bec1d6fb3fe58da98f044ad637a68ad1ff3891
rosinskiyda <rosinskiyda@intellect-soft.ru>
2024-12-08 08:28:52 +0400

    Спринт №9. Исправление замечаний.

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,39 +1,37 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.View
-import android.widget.EditText
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.example.playlistmaker.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
-    private var searchText: String = "";
+    private var searchText: String = ""
+    private lateinit var binding: ActivitySearchBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_search)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        findViewById<ImageView>(R.id.settings_arrow_back).setOnClickListener {
+        binding.settingsArrowBack.setOnClickListener {
             finish()
         }
 
-        val inputEditText = findViewById<EditText>(R.id.search_edit_text)
-        val clearButton = findViewById<ImageView>(R.id.clear_text)
-
-        clearButton.setOnClickListener {
-            inputEditText.setText(R.string.empty_string)
+        binding.clearText.setOnClickListener {
+            binding.searchEditText.setText(R.string.empty_string)
         }
 
         val simpleTextWatcher = object : TextWatcher {
@@ -42,7 +40,7 @@ class SearchActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                clearButton.visibility = getButtonVisibility(s)
+                binding.clearText.isVisible = !s.isNullOrEmpty()
                 searchText = if (s.isNullOrEmpty()) getString(R.string.empty_string) else s.toString()
             }
 
@@ -51,7 +49,7 @@ class SearchActivity : AppCompatActivity() {
             }
         }
 
-        inputEditText.addTextChangedListener(simpleTextWatcher)
+        binding.searchEditText.addTextChangedListener(simpleTextWatcher)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -63,16 +61,7 @@ class SearchActivity : AppCompatActivity() {
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
 
-        val text = savedInstanceState.getString(EDIT_TEXT_KEY)
-        findViewById<EditText>(R.id.search_edit_text).setText(text)
-    }
-
-    private fun getButtonVisibility(s: CharSequence?): Int {
-        return if (s.isNullOrEmpty()) {
-            View.GONE
-        } else {
-            View.VISIBLE
-        }
+        binding.searchEditText.setText(savedInstanceState.getString(EDIT_TEXT_KEY))
     }
 
     companion object {
