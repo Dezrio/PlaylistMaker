180e29d4a89fc85d91c42bae8cd1bdd0356387c3
Dezrio <Dezrio@yandex.ru>
2025-10-12 22:34:06 +0400

    Спринт №27 Отладка

--- app/src/main/java/com/example/playlistmaker/compose/AppColor.kt
+++ app/src/main/java/com/example/playlistmaker/compose/AppColor.kt
@@ -6,31 +6,28 @@ import androidx.compose.ui.graphics.Color
 
 val Black = Color(0xFF000000)
 val White = Color(0xFFFFFFFF)
-val Gray900 = Color(0xFF1A1B22)
-val Gray400 = Color(0xFFAEAFB4)
-val Gray50 = Color(0xFFE6E8EB)
-val Blue700 = Color(0xFF3772E7)
-val Blue200 = Color(0xFF9FBBF3)
-val Red300 = Color(0xFFF56B6C)
+val GrayMain = Color(0xFF1A1B22)
+val GraySecond = Color(0xFFAEAFB4)
+val GrayThird = Color(0xFFE6E8EB)
 
 val LightColorScheme = lightColorScheme(
     primary = White,
-    onPrimary = Gray900,
-    secondary = Gray50,
-    onSecondary = Gray400,
-    tertiary = Gray400,
-    onTertiary = Gray400,
+    onPrimary = GrayMain,
+    secondary = GrayThird,
+    onSecondary = GraySecond,
+    tertiary = GraySecond,
+    onTertiary = GraySecond,
     background = Color.White,
     onBackground = Color.Black,
-    surface = Gray900,
-    onSurface = Gray900
+    surface = GrayMain,
+    onSurface = GrayMain
 )
 
 val DarkColorScheme = darkColorScheme(
-    primary = Gray900,
+    primary = GrayMain,
     onPrimary = White,
     secondary = White,
-    onSecondary = Gray900,
+    onSecondary = GrayMain,
     tertiary = White,
     onTertiary = White,
     background = Color(0xFF121212),
